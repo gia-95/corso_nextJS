00301e0d5d478b9be25b44d2afd575e375cbeeaa
Gianluca <gia.marini95@gmail.com>
2022-11-21 15:59:56 +0100

    fine 'appendix' per TypeScript

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,18 +1,11 @@
-// import { User } from './User'
-// import { Company } from './Company';
+import { User } from './User'
+import { Company } from './Company';
+import { CustomMap } from "./CustomMap";
 
 
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
 
-// const user = new User();
-// console.log(user);
-
-// const company = new Company();
-// console.log(company);
-
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-});
+customMap.addMarker(user);
+customMap.addMarker(company);
